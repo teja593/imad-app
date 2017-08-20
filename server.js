7154b8e0e2c8c2c32a5b1002d62e5d977fb9fda5
teja593 <teja593@sasi.ac.in>
2017-08-20 11:26:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,18 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article -one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article -two',function(req,res){
-res.send("article two requested and will be server here");
-});
-
-app.get('/article -three',function(req,res){
-res.send("article threee requested and will be server here");
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
