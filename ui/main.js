20ca5bb61bad6b562accdea6b28c1c807f07c95c
teja593 <teja593@sasi.ac.in>
2017-08-22 16:46:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,20 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element=document.getElementById("main-text");
 element.innerHTML='new string updated';
+
+
+
+//to move the image
+
+var img=document.getElementById(img);
+img.onclick=function()
+{
+  img.style.marginleft='100px';
+  
+};
+
+
+
+
+
+
