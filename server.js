1fe7118f93516b5508ef00c78dd6740e70487812
teja593 <teja593@sasi.ac.in>
2017-08-22 06:39:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleName',function(req,res){
+app.get('/:articleName',function(req,res){
     //article name will be article-one
     var articleName=req.params.articleName;
      res.send(createTemplate(articles(articleName)));
